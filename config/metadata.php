85ec125dbdaf29a034621158881b7eaa438e1bbf
Chris Pymm <chris@chrispymm.co.uk>
2020-08-05 14:35:40 +0100

    add metadata and seotools configs

---
+++ config/metadata.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    'fallbacks' => [
+        'title' => 'title',
+        'description' => 'content',
+        'og_title' => 'title',
+        'og_description' => 'content',
+        'og_type' => 'metadataDefaultOgType',
+        'card_type' => 'metadataDefaultCardType',
+    ],
+
+    'opengraph_type_options' => [
+        ['value' => 'website', 'label' => 'Website',],
+        ['value' => 'article','label' => 'Article',],
+        ['value' => 'book', 'label' => 'Book',],
+        ['value' => 'profile', 'label' => 'Profile'],
+    ],
+
+    'card_type_options' => [
+        [ 'value' => 'summary', 'label' => 'Summary',],
+        [ 'value' => 'summary_large_image','label' => 'Summary with Large Image',],
+        [ 'value' => 'app', 'label' => 'App',],
+        [ 'value' => 'player', 'label' => 'Player'],
+    ],
+
+    'mediasParams' => [
+        'og_image' => [
+            'default' => [
+                [
+                    'name' => 'default',
+                    'ratio' => 1.91 / 1,
+                    'minValues' => [
+                        'width' => 1200,
+                        'height' => 627,
+                    ]
+                ]
+            ],
+
+        ]
+    ]
+];
